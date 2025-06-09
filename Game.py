85b5087ed2118c447e2f44e38e38ca60143a9e6e
Matias Pavez <kiorrvboy@gmail.com>
2025-06-09 17:34:43 -0400

    Actualización de historias sobre edades de Rey

--- Game.py
+++ Game.py
@@ -1,12 +1,16 @@
 from funGame import personajes
+import time
 
 
 print("\n-----  BIENVENID@  -----\n")
 continua = True
 
 print("...........................\n")
+time.sleep(1)
 print("En un mundo medieval, donde la caza de brujas es persistente y los pueblos luchan por comida.\n")
+time.sleep(1)
 print("Eres un.....\n")
+time.sleep(1)
 
 personajes()
 while continua:
@@ -22,16 +26,24 @@ while continua:
     
 if personaje == 1:
     print("¡Eres el nuevo rey del pueblo Nayhem!")
+    time.sleep(1)
     print("Tu antecesor fue asesinado en una guerra por el pueblo enemigo.")
+    time.sleep(1)
     nombre = input("Tu nombre es ..... ")
 
     print(f"Tu pueblo solo ha escuchado tu nombre.")
     print(nombre)
+    time.sleep(1)
     print("Cuando fuiste elegido, fuertes rumores circularon.")
+    time.sleep(1)
     print("Muchos piensan que eres un ogro, quien tomó el puesto por la fuerza.")
+    time.sleep(1)
     print("Otros, que eres un niño al quien le cumplieron su capricho de reinar.")
+    time.sleep(1)
     print("Pensaste que era absurdo.")
+    time.sleep(1)
     print("Tienes certeza de que no eres un ogro.")
+    time.sleep(1)
 
     edad = int(input("Tampoco eres un niño, tienes .... "))
 
